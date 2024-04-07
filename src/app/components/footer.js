53529d18e46b7bf0e93f3738632b087cc1f64bb1
Afnan Riaz <hafiz.afnan322@gmail.com>
2024-04-07 21:31:32 +0500

    made header footer responsive

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -11,14 +11,15 @@ export default function Footer() {
     return (
         <div className="h-[500px] relative text-white">
             <div className="bg-neutral-800 blur-xl absolute w-full h-full top-10 left-0"></div>
-            <div className="flex justify-between items-center pt-36 px-32 absolute z-20 top-0 left-0 h-full w-full">
+            <div className="flex flex-col sm:flex-row justify-between items-center pt-16 lg:pt-36 px-16 lg:px-32 gap-4 absolute z-20 top-0 left-0 h-full w-full">
                 <Image
                     src={"/images/logo-white.svg"}
                     height={180}
                     width={180}
                     alt="logo"
+                    className="lg:w-44 w-24"
                 />
-                <div className="flex gap-24">
+                <div className="grid grid-cols-[auto_auto] xs:grid-cols-[auto_auto_auto] gap-x-8 gap-y-4 lg:gap-24">
                     <div>
                         <h5 className="text-lg font-medium mb-5">Site map</h5>
                         <ul className="text-sm leading-8">
@@ -36,11 +37,11 @@ export default function Footer() {
                             <li>Location: Lahore, Pakistan</li>
                         </ul>
                     </div>
-                    <div>
+                    <div className="xs:col-span-1 col-span-2">
                         <h5 className="text-lg font-medium mb-5">
                             Social Links
                         </h5>
-                        <div className="text-sm grid gap-y-4 grid-cols-2">
+                        <div className="text-sm grid gap-4 grid-cols-[40px_40px_40px_40px] xs:grid-cols-2">
                             <FacebookSVG />
                             <InstagramSVG />
                             <YoutubeSVG />
--- src/app/components/header.js
+++ src/app/components/header.js
@@ -7,7 +7,7 @@ export default function Header() {
         if (cheveron) cheveron.style.transform = "rotate(180deg)";
         const item = e.currentTarget.querySelector(".navItem");
         const menu = item.querySelector(".navMenu");
-        if (menu){
+        if (menu) {
             menu.style.maxHeight = "160px";
         }
     };
@@ -16,21 +16,24 @@ export default function Header() {
         if (cheveron) cheveron.style.transform = "rotate(0deg)";
         const item = e.currentTarget.querySelector(".navItem");
         const menu = item.querySelector(".navMenu");
-        if (menu){
+        if (menu) {
             menu.style.maxHeight = "0px";
         }
     };
     return (
-        <header className="text-white z-10 relative h-32">
-            <div className="absolute -top-32 left-96 -z-10 min-w-[300px] min-h-[300px] blur-3xl rounded-full bg-opacity-40 bg-red-600"></div>
-            <nav className="h-full flex px-14 justify-between items-center">
+        <header className="text-white z-10 relative h-20 xs:h-32">
+            <div className="max-[300px]:hidden absolute -top-32 left-0 sm:left-[24vw] -z-10 min-w-[300px] min-h-[300px] blur-3xl rounded-full bg-opacity-40 bg-red-600"></div>
+            <nav className="h-full flex px-4 sm:px-8 xl:px-14 justify-between items-center">
                 <Link href="/">
-                <h1 className="mr-2 cursor-pointer uppercase font-orbitron text-[40px] font-black text-red-primary">
-                    break into pcs
-                </h1>
+                    <h1 className="mr-2 cursor-pointer uppercase font-orbitron text-2xl sm:text-3xl xl:text-[40px] font-black text-red-primary">
+                        break into pcs
+                    </h1>
                 </Link>
-                <div className="flex items-center gap-[6vw] ml-auto mr-8">
-                    <Link className="hidden lg:block text-lg font-medium hover:text-gray-200" href="/">
+                <div className="flex items-center lg:gap-10 xl:gap-[5vw]">
+                    <Link
+                        className="hidden lg:block text-lg font-medium hover:text-gray-200"
+                        href="/"
+                    >
                         Home
                     </Link>
                     <div
@@ -39,7 +42,10 @@ export default function Header() {
                         className="hidden lg:block relative"
                     >
                         <div className="navItem flex flex-col">
-                            <Link className="flex gap-2 text-lg font-medium hover:text-gray-200" href="/products">
+                            <Link
+                                className="flex gap-2 text-lg font-medium hover:text-gray-200"
+                                href="/products"
+                            >
                                 Products
                                 <Image
                                     alt="photo"
@@ -53,15 +59,21 @@ export default function Header() {
                                 <ul className="m-4">
                                     <li className="flex gap-1 leading-8 items-center">
                                         <div className="h-6 w-1 rounded-sm bg-red-primary"></div>
-                                        <Link className="hover:text-gray-200" href={"#"}>Gaming PCs and parts</Link>
+                                        <Link
+                                            className="hover:text-gray-200"
+                                            href={"/products/marketplace"}
+                                        >
+                                            Gaming PCs and parts
+                                        </Link>
                                     </li>
                                     <li className="flex gap-1 leading-8 items-center">
                                         <div className="h-6 w-1 rounded-sm bg-red-primary"></div>
-                                        <Link className="hover:text-gray-200" href={"/products/games"}>Games</Link>
-                                    </li>
-                                    <li className="flex gap-1 leading-8 items-center">
-                                        <div className="h-6 w-1 rounded-sm bg-red-primary"></div>
-                                        <Link className="hover:text-gray-200" href={"#"}>Accessories</Link>
+                                        <Link
+                                            className="hover:text-gray-200"
+                                            href={"/products/games"}
+                                        >
+                                            Softwares and Games
+                                        </Link>
                                     </li>
                                 </ul>
                             </div>
@@ -73,7 +85,10 @@ export default function Header() {
                         className="hidden lg:block relative"
                     >
                         <div className="navItem flex flex-col">
-                            <Link className="flex gap-2 text-lg font-medium hover:text-gray-200" href="/resources">
+                            <Link
+                                className="flex gap-2 text-lg font-medium hover:text-gray-200"
+                                href="/resources"
+                            >
                                 Resourses
                                 <Image
                                     alt="photo"
@@ -87,40 +102,45 @@ export default function Header() {
                                 <ul className="m-4">
                                     <li className="flex gap-1 leading-8 items-center">
                                         <div className="h-6 w-1 rounded-sm  hover:text-gray-200 bg-red-primary"></div>
-                                        <Link className="hover:text-gray-200" href="/resources/builder">Custom PC Builder</Link>
+                                        <Link
+                                            className="hover:text-gray-200"
+                                            href="/resources/builder"
+                                        >
+                                            Custom PC Builder
+                                        </Link>
                                     </li>
                                     <li className="flex gap-1 leading-8 items-center">
                                         <div className="h-6 w-1 rounded-sm  hover:text-gray-200 bg-red-primary"></div>
-                                        <Link className="hover:text-gray-200" href="#">Benchmarking Tool</Link>
-                                    </li>
-                                    <li className="flex gap-1 leading-8 items-center">
-                                        <div className="h-6 w-1 rounded-sm  hover:text-gray-200 bg-red-primary"></div>
-                                        <Link className="hover:text-gray-200" href="#">System Check Tool</Link>
-                                    </li>
-                                    <li className="flex gap-1 leading-8 items-center">
-                                        <div className="h-6 w-1 rounded-sm  hover:text-gray-200 bg-red-primary"></div>
-                                        <Link className="hover:text-gray-200" href="/resources/blogs">Tech Blogs</Link>
+                                        <Link
+                                            className="hover:text-gray-200"
+                                            href="/resources/blogs"
+                                        >
+                                            Tech Blogs
+                                        </Link>
                                     </li>
                                 </ul>
                             </div>
                         </div>
                     </div>
-                    <Link className="hidden lg:block text-lg font-medium hover:text-gray-200" href="#">
+                    <Link
+                        className="hidden lg:block text-lg font-medium hover:text-gray-200"
+                        href="/about"
+                    >
                         About Us
                     </Link>
-                    <Link href="#">
-                        <button className="transition-all duration-500 h-[43px] w-28 min-w-[75px] min-h-[35px] bg-red-gradient rounded-md  bg-size-200 bg-center text-lg font-bold hover:bg-pos-0">
-                        Sign Up
+                    <Link href="#" className="hidden xs:block">
+                        <button className="transition-all duration-500 h-[43px] sm:w-28 w-24 min-h-[35px] bg-red-gradient rounded-md  bg-size-200 bg-center text-lg font-bold hover:bg-pos-0">
+                            Sign Up
                         </button>
                     </Link>
-                </div>
-                <div className="inline lg:hidden ml-6">
-                    <Image
-                        alt="photo"
-                        src={"/icons/hamburger.svg"}
-                        width={30}
-                        height={30}
-                    />
+                    <div className="inline lg:hidden xs:ml-6 w-6">
+                        <Image
+                            alt="photo"
+                            src={"/icons/hamburger.svg"}
+                            width={30}
+                            height={30}
+                        />
+                    </div>
                 </div>
             </nav>
         </header>
